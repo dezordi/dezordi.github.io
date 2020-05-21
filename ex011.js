f878e0833e2762e8ae721e4405b8e4678b12abf0
dezordi <zimmer.filipe@gmail.com>
2020-05-21 19:10:46 -0300

    new figures html

--- ex011.js
+++ ex011.js
@@ -6,15 +6,15 @@ function carregar(){
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
        //BOM DIA 
-       img.src = 'manha.png'
+       img.src = 'https://github.com/dezordi/dezordi.github.io/blob/master/_images/manha.png?raw=true'
        document.body.style.background = '#b9846f'
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE
-        img.src = 'tarde.png'
+        img.src = 'https://github.com/dezordi/dezordi.github.io/blob/master/_images/tarde.png?raw=true'
         document.body.style.background = '#f2a153'
     } else {
         //BOA NOITE
-        img.src = 'noite.png'
+        img.src = 'https://github.com/dezordi/dezordi.github.io/blob/master/_images/noite.png?raw=true'
         document.body.style.background = '#515154'
     }
 }
